3b5b0750cff8146290356cc457db7808cf6c4960
liushui1986 <liushui1986@SHUAIPENG.localdomain>
2024-08-10 15:36:09 -0500

    literature analysis chatbot-test1

---
+++ chatbot_08102024/chatbot_08102024.py
@@ -0,0 +1,62 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.chat_models import ChatOpenAI
+
+
+OPENAI_API_KEY = "input your key here"
+
+# Upload PDF files
+st.header("My literature analysis Chatbot")
+
+with st.sidebar:
+    st.title("Your Documents")
+    file = st.file_uploader("Upload a PDF file and start asking questions", type="pdf")
+
+# Extract the text
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+#       st.write(text)
+
+# Break it into chunks and you can adjust there parameters yourself.
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators="\n",
+        chunk_size=1000,
+        chunk_overlap=150,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    st.write(chunks)
+
+# Generating embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+# Creating vector store  - FAISS
+    vector_store = FAISS.from_texts(chunks, embeddings)
+
+# Get user question
+    user_question = st.text_input("Type Your Question Here")
+
+# Do similarity search
+    if user_question:
+        match = vector_store.similarity_search(user_question)
+#       st.write(match)
+
+# define the LLM, and fine-tune those numbers
+        llm = ChatOpenAI(
+            openai_api_key=OPENAI_API_KEY,
+            temperature=0,
+            max_tokens=1000,
+            model_name="gpt-3.5-turbo"
+        )
+
+# Output results
+        chain = load_qa_chain(llm, chain_type="stuff")
+        response = chain.run(input_documents=match, question=user_question)
+        st.write(response)
