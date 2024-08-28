9c9ac7c703290d85f5b18105a83077e113a010eb
liushui1986 <liushui1986@SHUAIPENG.localdomain>
2024-08-28 15:32:18 -0500

    update requirements.txt

--- chatbot_08102024/chatbot_08102024.py
+++ chatbot_08102024/chatbot_08102024.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.chat_models import ChatOpenAI
 
 
-OPENAI_API_KEY = "input your key here"
+OPENAI_API_KEY = "use your own api key here"
 
 # Upload PDF files
 st.header("My literature analysis Chatbot")
